18a225e0373e4204641740dc6215162f5f8b8c96
timoderbeste <ntwang1994@gmail.com>
2019-11-11 13:20:46 -0600

    Adding tests for partydialog

---
+++ play-our-song/src/components/PartyDialog.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PartyDialog from './components/PartyDialog';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<PartyDialog />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
