0deb34b845e2610b20dbaa4ad6ffd162c5719220
Timo Wang <ntwang1994@gmail.com>
2019-12-08 19:01:57 -0600

    Adding interactive test

--- play-our-song/src/components/PartyDialog.test.js
+++ play-our-song/src/components/PartyDialog.test.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {render, fireEvent} from 'react-testing-library';
+import {render, fireEvent} from '@testing-library/react';
 import PartyDialog from './PartyDialog';
+import { StoreContext } from '../utils/store';
+
 
 
 it('renders without crashing', () => {
-  const ctx = React.useContext(StoreContext);
+  const open = jest.fn();
 
   const {getByTestId} = render(<PartyDialog />);
   expect(getByTestId('party-dialog-content').textContent).toBe('To generate your playlist, please enter the title of your playlist and the type of your event.');
   fireEvent.click(getByTestId('generate-button'));
-  expect(ctx.open[1]).toHaveBeenCalledTimes(1);
+  expect(open).toBeCalledWith(false);
+  expect(open).toHaveBeenCalledTimes(1);
 });
